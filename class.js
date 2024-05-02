665f71a199de13c9d823e182b551f3384934e82d
Mtdung290504 <mtdung114290504@gmail.com>
2024-05-02 22:17:08 +0700

    2/5/2024 10:17PM

--- class.js
+++ class.js
@@ -17,31 +17,31 @@ router.get(
         const rootUrl = Utils.getRootUrl(req);
 
         try {
-            const { class_name, member_count } = await db.getClassNameAndMember(
-                classId
-            );
+            const { class_name, member_count } = await db.getClassNameAndMember(classId);
             const listOfClasses = await db.getAllClass(user.id, role);
 
             if (role == 1) {
                 const listOfAttachFile = await db.getClassAttachFiles(classId, user.id);
                 res.render("class-views/lecturer", {
-                    rootUrl,
-                    user,
+                    rootUrl, user,
                     role: roles[role],
                     listOfAttachFile,
                     listOfClasses,
                     className: class_name,
                     members: member_count
                 });
-                // console.log({
-                //     classId,
-                //     role,
-                //     className: class_name,
-                //     members: member_count,
-                //     listOfAttachFile,
-                // });
                 return;
             }
+            const lecturerId = listOfClasses[listOfClasses.findIndex(cls => cls.id == classId)]['lecturer_id'];
+            const listOfAttachFile = await db.getClassAttachFiles(classId, lecturerId);
+            res.render("class-views/student", {
+                rootUrl, user,
+                role: roles[role],
+                listOfAttachFile,
+                listOfClasses,
+                className: class_name,
+                members: member_count
+            });
         } catch (error) {
             console.error(error);
             res.send("Internal server error");
